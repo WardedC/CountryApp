1522590bd366cd8f55cb232bc317e412b44c6faa
Edward Cabrera <93951069+WardedC@users.noreply.github.com>
2024-01-25 12:48:08 -0400

    Organization and created aboutPageComponent and homePageComponent

---
+++ src/app/shared/pages/aboutPage/aboutPage.component.ts
@@ -0,0 +1,10 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'shared-about-page',
+    templateUrl: './aboutPage.component.html',
+    styleUrl: './aboutPage.component.html'
+
+})
+export class AboutPageComponent { }
