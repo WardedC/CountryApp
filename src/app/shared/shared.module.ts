8efff8791c9b9e93fe9a01414d806c407b69457a
Edward Cabrera <93951069+WardedC@users.noreply.github.com>
2024-01-31 11:50:13 -0500

    Slide bar with routes navigation

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,21 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { HomePageComponent } from './pages/homePage/homePage.component';
 import { AboutPageComponent } from './pages/aboutPage/aboutPage.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
 
   exports:[
     HomePageComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    SidebarComponent
   ]
 })
 export class SharedModule { }
